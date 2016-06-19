d863c43be03f71762476b80530d8b2a29e9c1412
Wentao MA <mail.walter.ma@gmail.com>
2016-06-19 16:55:21 +0800

    GameController

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,7 +6,10 @@ public class GameController : MonoBehaviour {
     public GameObject Menu;
     public GameObject Level1;
     public GameObject Level2;
-    public GameObject HandAttachment;
+	public Leap.Unity.HandAttachments handAttachment;
+
+	private GameObject _level1;
+	private GameObject _level2;
 
 	public void hideMenu()
     {
@@ -17,4 +20,29 @@ public class GameController : MonoBehaviour {
     {
         Menu.SetActive(true);
     }
+
+	public void startNormalMode(){
+		if(_level1 != null){
+			Destroy (_level1);
+		}
+		if (_level2 != null) {
+			Destroy (_level2);
+		}
+		_level1 = GameObject.Instantiate (Level1);
+		_level2 = GameObject.Instantiate (Level2);
+		handAttachment.enabled = true;
+	}
+
+	public void startFreeMode(){
+		if(_level1 != null){
+			Destroy (_level1);
+		}
+		if (_level2 != null) {
+			Destroy (_level2);
+		}
+		handAttachment.enabled = true;
+	}
+
+	public void exitGame(){
+	}
 }
